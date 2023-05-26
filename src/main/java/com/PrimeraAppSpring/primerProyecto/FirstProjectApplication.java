b034686b61e48631370c6f274860791c997d0fda
Ezequiel Fleitas <ezequielfleitas040@gmail.com>
2023-05-26 07:57:27 -0300

    Commit de propierties

--- src/main/java/com/PrimeraAppSpring/primerProyecto/FirstProjectApplication.java
+++ src/main/java/com/PrimeraAppSpring/primerProyecto/FirstProjectApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstProjectApplication.class, args);
-	}
+    public String PORT = System.getenv("jdbc:mysql://uwzqkn5lkxbsm9co:8WQP4PYCDO1pjRBzrIOZ@bxx3q63s3qwldpqwlony-mysql.services.clever-cloud.com:3306/bxx3q63s3qwldpqwlony");
+  
+    public static void main(String[] args) {
+            SpringApplication.run(FirstProjectApplication.class, args);
+    }
 
 }
